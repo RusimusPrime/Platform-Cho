5eb7dd144fc9b4cec1518461b81a87e4453ba922
RusimusPrime <ruslankosov9@gmail.com>
2025-05-12 18:29:54 +0300

    почти сайт

--- Test.py
+++ Test.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, render_template_string
-
+from flask import Flask, request, render_template_string, render_template, redirect
+import hashlib
 import os
 from PyPDF2 import PdfReader
 import sqlite3
 from striprtf.striprtf import rtf_to_text
+
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
+AUTHORIZED = False
 
 
 @app.route("/load", methods=["GET", "POST"])
@@ -40,7 +42,7 @@ def upload_pdf():
     """)
 
 
-@app.route('/<id>')
+@app.route('/watch/<id>')
 def show_file(id):
     connection = sqlite3.connect('cho.db')
     cur = connection.cursor()
@@ -62,6 +64,48 @@ def books():
     result = cur.execute("""SELECT id, name FROM bible""").fetchall()
     print(result)
     return result
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        connection = sqlite3.connect('cho.db')
+        cur = connection.cursor()
+        result = cur.execute("""SELECT * FROM users""").fetchall()
+        cur.execute('INSERT INTO users (id, login, password) VALUES (?, ?, ?)',
+                    (len(result) + 1, request.form.get('userName'),
+                     hashlib.sha256(request.form.get('password').encode()).hexdigest(),))
+        connection.commit()
+        connection.close()
+        return redirect("/home", code=302)
+    return render_template('register_window.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/singing', methods=['GET', 'POST'])
+def singing():
+    global AUTHORIZED
+    if request.method == 'POST':
+        try:
+            connection = sqlite3.connect('cho.db')
+            cur = connection.cursor()
+            result = cur.execute("""SELECT login FROM users where password = ?""",
+                                 (hashlib.sha256(request.form.get('password').encode()).hexdigest(),)).fetchall()[0][0]
+            print(result)
+            connection.close()
+        except TypeError:
+            return "Ничего не нашлось"
+        except IndexError:
+            return "Ничего не нашлось"
+        else:
+            if result == request.form.get('userName'):
+                AUTHORIZED = True
+                return redirect("/home", code=302)
+            else:
+                return "неправиьный логин"
+    return render_template('singing in account.html')
 
 
 def action(name):
